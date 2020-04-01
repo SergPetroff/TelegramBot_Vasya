a7db2453c3b3af9a5409db4e4cbfd6299d2db1bb
SERGEY PETROV <petrovmal@gmail.com>
2020-04-01 14:59:54 +0300

    add port heroku

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const Telegraf = require('telegraf');
 const covidService = require("./services/covid");
 const formatCountryMsg = require("./messages/country");
+const express = require('express');
+const expressApp = express();
+
 const BOT_TOKEN = process.env.BOT_TOKEN || "95669930:AAFufAdJdpOtMLRTUlzOM3twxLzBq-geZHE"
+
+const port = process.env.PORT || 3000;
+
+expressApp.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  expressApp.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+  })
 
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.start(ctx => ctx.reply(`
 Добро пожаловать в COVID19 Bot!
-Вам нужно отправить название стрнаы что бы получить статистику.
+Вам нужно отправить название страны что бы получить статистику.
 `));
 
 bot.help(ctx => ctx.reply(`Например:
@@ -20,7 +32,7 @@ bot.hears(/.*/, async ctx => {
     if(data && data.results===0){
         return ctx.reply(`Страна не найдена`)
     }
-    console.log(`Country:${data.country}`)
+    console.log(`Country:${data.response[0].country}`)
     return ctx.replyWithMarkdown(formatCountryMsg(data.response[0]))
 });
 
